130e4dc39b49904619adb49ff5ac712c2af1238c
Sebastiaan van Krieken <48060248+Sebastiaanvk@users.noreply.github.com>
2024-06-13 11:32:05 +0200

    Finished Day5 2023

--- Advent of Code 2023/Day 5.py
+++ Advent of Code 2023/Day 5.py
@@ -16,28 +16,41 @@ part1()
 
 def part2():
     input = open("Day 5.txt","r").read().split("\n\n")
+    #input = open("test5.txt","r").read().split("\n\n")
+
     seeds_prim = list(map(int,input[0].split()[1:]))
     i = 0
     seeds = []
     while i<len(seeds_prim):
-        seeds.append((seeds_prim[i],seeds_prim[1]))
+        seeds.append((seeds_prim[i],seeds_prim[i]+seeds_prim[i+1]-1))
         i += 2
-
+    print(seeds)
     for i in range(1,len(input)):
-        new_seeds = []
         l = list(map(lambda x: list(map(int,x.split())),input[i].split("\n")[1:]))
-        for j in range(len(seeds)):
-            start = seeds[j][0]
-            length = seeds[j][1]
-            for x in l:
-                if start in range(x[1],x[1]+x[2]) and start+length-1 in range(x[1,x[1]+x[2]]):
- 
-                else if:
-
-                else if:
-        
-        new_seeds
-        seeds = new_seeds
-    print("Part2:", min(list(map(lambda x: x[0],seeds)))
+        processed_seeds = []
+        for window in l:
+            next_current_seeds = []
+            left = window[1]
+            right = left + window[2]-1
+            augment = window[0]-window[1]
+            for p in seeds:
+                x,y = p[0],p[1]
+                if y<left or x >right:
+                    next_current_seeds.append(  (x,y))
+                elif x<left and y<=right:
+                    next_current_seeds.append((x,left-1))
+                    processed_seeds += [(left+augment,y+augment)]
+                elif x>=left and y<= right:
+                    processed_seeds += [(x+augment,y+augment)]
+                elif x<left and y>right:
+                    processed_seeds += [(left+augment,right+augment)]
+                    next_current_seeds += [(x,left-1)]
+                    next_current_seeds += [(right+1,y)]
+                elif x>=left and y>right:
+                    processed_seeds += [(x+augment,right+augment)]
+                    next_current_seeds += [(right+1,y)]
+            seeds = next_current_seeds
+        seeds += processed_seeds
+    print("Part2:", min(list(map(lambda x: x[0],seeds))))
 
 part2()
